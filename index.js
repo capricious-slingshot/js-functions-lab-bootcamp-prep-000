33b6677893a727b5e244ba82f0f6e7fc7b5e975e
Cara D <ephemere@protonmail.com>
2018-08-31 04:13:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ function happyHolidays(){
 }
 
 function happyHolidaysTo(name){
-  return "Happy Holidays, ${name}!"
+  return `Happy Holidays, ${name}!`
 }
 
 function happyHolidayTo(name, holiday){
-  return "Happy ${holiday}, ${name}!"
+  return `Happy ${holiday}, ${name}!`
 }
 
 function holidayCountdown(holiday, days){
-  return "It's ${days} days until ${holiday}!"
+  return `It's ${days} days until ${holiday}!`
 }
